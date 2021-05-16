29d777ba5acdba3fc1332ac8184d3ab04880c6c0
Tom Petrangelo <tdpetrangelo@gmail.com>
2021-05-16 20:06:19 +0100

    update to js polling

--- js/download_current_song.js
+++ js/download_current_song.js
@@ -6,7 +6,7 @@ async function downloadedSong(url) {
   const response = await fetch(url);
   const buffer = await response.buffer();
   try {
-    const data = fs.writeFileSync(path.resolve(__dirname, "../../images/current_song.jpg"), buffer)
+    const data = fs.writeFileSync(path.resolve(__dirname, "../../Music_Matrix/images/current_song.jpg"), buffer)
   } catch (err) {
     console.error(err)
   }
--- js/poll_playing.js
+++ js/poll_playing.js
@@ -7,12 +7,14 @@ async function getCurrentSong(spotifyApi) {
   let response = await spotifyApi.getMyCurrentPlayingTrack("US");
 
   if (response.statusCode == 502) {
+      console.log(response.statusCode);
     var newSpotifyApi = new SpotifyWebApi({
       accessToken: spotifyApi.getAccessToken(),
     });
 
     await getCurrentSong(newSpotifyApi);
   } else if (response.statusCode != 200) {
+      console.log(response.statusCode);
     var newSpotifyApi = new SpotifyWebApi({
       accessToken: spotifyApi.getAccessToken(),
     });
@@ -23,33 +25,36 @@ async function getCurrentSong(spotifyApi) {
     await new Promise((resolve) => setTimeout(resolve, 5000));
     await getCurrentSong(newSpotifyApi);
   } else {
+      //console.log(response.body);
+      
 
-    if (currAlbumURI != response.body.context.uri){
+    if (currAlbumURI != response.body.item.uri){
       console.log("")
       console.log("New Album Playing")
       console.log("------------------------------------")
       console.log("Old Album: " + currAlbumURI);
-      console.log("New Album: " + response.body.context.uri);
+      console.log("New Album: " + response.body.item.uri);
       console.log("------------------------------------")
 
       let songURL = response.body.item.album.images[2].url;
       let currentSongID = response.body.item.id;
       await songImage.downloadedSong(songURL);
+      
     } else {
         console.log("");
         console.log("Same Album Playing");
         console.log("------------------------------------");
         console.log("Old ID: " + currAlbumURI);
-        console.log("New ID: " + response.body.context.uri);
+        console.log("New ID: " + response.body.item.uri);
         console.log("------------------------------------");
     }
 
-    var newSpotifyApi = new SpotifyWebApi({
-      accessToken: spotifyApi.getAccessToken(),
-    });
-    currAlbumURI = response.body.context.uri;
-    await new Promise((resolve) => setTimeout(resolve, 10000));
-    await getCurrentSong(newSpotifyApi);
+    //var newSpotifyApi = new SpotifyWebApi({
+    //  accessToken: spotifyApi.getAccessToken(),
+    //});
+    currAlbumURI = response.body.item.uri;
+    await new Promise((resolve) => setTimeout(resolve, 7500));
+    await getCurrentSong(spotifyApi);
   }
 }
 
