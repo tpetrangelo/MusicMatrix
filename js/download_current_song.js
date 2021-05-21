0bdac0d9c166911c486ef78026ae0f6711dd5c37
Tom Petrangelo <tdpetrangelo@gmail.com>
2021-05-21 02:08:35 +0100

    added Jar file to help deepen blacks

--- js/download_current_song.js
+++ js/download_current_song.js
@@ -1,15 +1,22 @@
 const fs = require('fs');
 const fetch = require('node-fetch');
 const path = require("path");
-const { exec } = require("child_process");
+const { exec, execSync } = require("child_process");
 const matrix = require("./prepare_new_matrix");
+
+const options = {
+
+    cwd: '/home/pi/Desktop/Music_Matrix/java/'
+
+};
 
 async function downloadedSong(url) {
   const response = await fetch(url);
   const buffer = await response.buffer();
   try {
-    fs.writeFileSync(path.resolve(__dirname, "../../Music_Matrix/images/current_song.jpg"), buffer)
-    exec("mogrify -format ppm current_song.jpg", { cwd: '/home/pi/Desktop/Music_Matrix/images/' }, (error, stdout, stderr) => {
+    fs.writeFileSync(path.resolve(__dirname, "../../Music_Matrix/images/current_song.png"), buffer)
+      execSync('java -jar ../java/Pixelize.jar', options); 
+    exec("mogrify -format ppm current_song.png", { cwd: '/home/pi/Desktop/Music_Matrix/images/' }, (error, stdout, stderr) => {
       if (error) {
         console.log('error in download_current_song: ' + error.message);
         return;
